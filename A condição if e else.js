5439980adb21f619ade3c5362d2e8ea96e5e9cae
Bianca Martinelli <bianca.amartinelli@gmail.com>
2022-12-09 00:25:57 -0300

    Exercicios

---
+++ A condição if e else.js
@@ -0,0 +1,11 @@
+var senha = prompt("Entre com sua senha");
+
+if(senha == "calopsitaazul") {
+
+    alert("Acesso liberado!");
+} else {
+
+    alert("Acesso negado!");
+}
+
+
---
+++ O resultado é.js
@@ -0,0 +1,7 @@
+var a = parseInt("10");
+var b = parseInt("Calopsita");
+var c = parseInt(10);
+
+/*A variável `a` recebe o número 10 retornado por `parseInt`.
+A variável `b` recebe `NaN` (not a number) retornado por `parseInt`.
+A variável `c` recebe 10, número, retornado por `parseInt`.*/
